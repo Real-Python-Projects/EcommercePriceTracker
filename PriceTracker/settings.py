71d561103211e27ba89b1586179ac13aa278be8b
Gateway47 <itsregalo047@gmail.com>
2021-04-15 20:40:15 +0300

    added User models

--- PriceTracker/settings.py
+++ PriceTracker/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'ProductsApp',
     'User',
+    
+    'imagekit',
 ]
 
 MIDDLEWARE = [
@@ -130,3 +132,5 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = [
     os.path.join(BASE_DIR, 'media')
 ]
+
+AUTH_USER_MODEL = "User.CustomUser"
--- User/models.py
+++ User/models.py
@@ -1,3 +1,58 @@
 from django.db import models
-
+from django.contrib.auth.models import AbstractUser
+from django.dispatch import receiver
+from django.urls import reverse
+from PIL import Image
+from imagekit.models import ImageSpecField
+from pilkit.processors import ResizeToFill
 # Create your models here.
+
+
+class CustomUser(AbstractUser):
+    user_type_choices = ((1,"Admin"),(2,"Staff"),(3,"Merchant"),(4,"Customer"))
+    user_type = models.CharField(max_length=255, choices=user_type_choices, default=1)
+    
+    
+class AdminUser(models.Model):
+    profile_pic=models.ImageField(upload_to="images/profile/admin")
+    profile_pic_thumbnail = ImageSpecField(source='profile_pic',
+                                           processors=[ResizeToFill(100,100)],
+                                           format='JPEG',
+                                           options={'quality':100}
+                                           )
+    auth_user_id = models.OneToOneField(CustomUser, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    
+class StaffUser(models.Model):
+    profile_pic=models.ImageField(upload_to="images/profile/staff")
+    profile_pic_thumbnail = ImageSpecField(source='profile_pic',
+                                           processors=[ResizeToFill(100,100)],
+                                           format='JPEG',
+                                           options={'quality':100}
+                                           )
+    auth_user_id = models.OneToOneField(CustomUser, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+class MerchantUser(models.Model):
+    profile_pic=models.ImageField(upload_to="images/profile/merchant")
+    profile_pic_thumbnail = ImageSpecField(source='profile_pic',
+                                           processors=[ResizeToFill(100,100)],
+                                           format='JPEG',
+                                           options={'quality':100}
+                                           )
+    auth_user_id = models.OneToOneField(CustomUser, on_delete=models.CASCADE)
+    company_name = models.CharField(max_length=255)
+    address = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+class CustomerUser(models.Model):
+    profile_pic=models.ImageField(upload_to="images/profile/customer")
+    profile_pic_thumbnail = ImageSpecField(source='profile_pic',
+                                           processors=[ResizeToFill(100,100)],
+                                           format='JPEG',
+                                           options={'quality':100}
+                                           )
+    auth_user_id = models.OneToOneField(CustomUser, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    
