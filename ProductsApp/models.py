b4252a2e421694c4e4df78b3f7b0656e22a21737
Gateway47 <itsregalo047@gmail.com>
2021-04-15 21:09:34 +0300

    created products models

--- ProductsApp/models.py
+++ ProductsApp/models.py
@@ -1,3 +1,151 @@
 from django.db import models
-
+from django.urls import reverse
+from django.utils.text import slugify
+from User.models import MerchantUser, CustomerUser
 # Create your models here.
+
+class Categories(models.Model):
+    title=models.CharField(max_length=255)
+    slug=models.SlugField()
+    thumbnail = models.ImageField(upload_to="images/products/categories/")
+    description=models.TextField()
+    created_at=models.DateTimeField(auto_now_add=True)
+    is_active = models.IntegerField(default=1)
+    
+    def get_absolute_url(self):
+        return reverse("products:category-list-page")
+
+    def save(*args, **kwargs):
+        if not self.slug:
+            self.slug=slugify(self.title)
+        return super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.title
+    
+class SubCategories(models.Model):
+    category_id=models.ForeignKey(Categories,on_delete=models.CASCADE)
+    title=models.CharField(max_length=255)
+    slug=models.SlugField(blank=True)
+    thumbnail = models.ImageField(upload_to="images/products/subcategories")
+    description=models.TextField()
+    created_at=models.DateTimeField(auto_now_add=True)
+    is_active = models.IntegerField(default=1)
+    
+    def get_absolute_url(self):
+        return reverse("products:sub-category-list-page")
+
+    def save(*args, **kwargs):
+        if not self.slug:
+            self.slug=slugify(self.title)
+        return super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.title
+
+
+class Products(models.Model):
+    slug=models.SlugField(blank=True)
+    subcategories_id=models.ForeignKey(SubCategories,on_delete=models.CASCADE)
+    product_name=models.CharField(max_length=255)
+    brand=models.CharField(max_length=255)
+    product_max_price=models.CharField(max_length=255)
+    product_discount_price=models.CharField(max_length=255)
+    product_description=models.TextField()
+    product_long_description=models.TextField()
+    created_at=models.DateTimeField(auto_now_add=True)
+    added_by_merchant=models.ForeignKey(MerchantUser,on_delete=models.CASCADE)
+    in_stock_total=models.IntegerField(default=1)
+    is_active=models.IntegerField(default=1)
+    
+    def save(*args, **kwargs):
+        if not self.slug:
+            self.slug=slugify(self.product_name)
+        return super().save(*args, **kwargs)
+    
+    
+class ProductMedia(models.Model):
+    product_id=models.ForeignKey(Products,on_delete=models.CASCADE)
+    media_type_choice=((1,"Image"),(2,"Video"))
+    media_type=models.CharField(max_length=255)
+    media_content=models.FileField(upload_to="product_media")
+    created_at=models.DateTimeField(auto_now_add=True)
+    is_active=models.IntegerField(default=1) 
+    
+    
+class ProductTransaction(models.Model):
+    transaction_type_choices=((1,"BUY"),(2,"SELL"))
+    product_id=models.ForeignKey(Products,on_delete=models.CASCADE)
+    transaction_product_count=models.IntegerField(default=1)
+    transaction_type=models.CharField(choices=transaction_type_choices,max_length=255)
+    transaction_description=models.CharField(max_length=255)
+    created_at=models.DateTimeField(auto_now_add=True) 
+    
+class ProductDetails(models.CharField):
+    product_id=models.ForeignKey(Products,on_delete=models.CASCADE)
+    title=models.CharField(max_length=255)
+    title_details=models.CharField(max_length=255)
+    created_at=models.DateTimeField(auto_now_add=True)
+    is_active=models.IntegerField(default=1)
+    
+    
+class ProductAbout(models.CharField):
+    product_id=models.ForeignKey(Products,on_delete=models.CASCADE)
+    title=models.CharField(max_length=255)
+    created_at=models.DateTimeField(auto_now_add=True)
+    is_active=models.IntegerField(default=1)
+
+class ProductTags(models.Model):
+    product_id=models.ForeignKey(Products,on_delete=models.CASCADE)
+    title=models.CharField(max_length=255)
+    created_at=models.DateTimeField(auto_now_add=True)
+    is_active=models.IntegerField(default=1)
+
+class ProductQuestions(models.Model):
+    product_id=models.ForeignKey(Products,on_delete=models.CASCADE)
+    user_id=models.ForeignKey(CustomerUser,on_delete=models.CASCADE)
+    question=models.TextField()
+    answer=models.TextField()
+    created_at=models.DateTimeField(auto_now_add=True)
+    is_active=models.IntegerField(default=1)
+
+class ProductReviews(models.Model):
+    product_id=models.ForeignKey(Products,on_delete=models.CASCADE)
+    user_id=models.ForeignKey(CustomerUser,on_delete=models.CASCADE)
+    review_image=models.FileField()
+    rating=models.CharField(default="5",max_length=255)
+    review=models.TextField(default="")
+    created_at=models.DateTimeField(auto_now_add=True)
+    is_active=models.IntegerField(default=1)
+
+class ProductReviewVoting(models.Model):
+    product_review_id=models.ForeignKey(ProductReviews,on_delete=models.CASCADE)
+    user_id_voting=models.ForeignKey(CustomerUser,on_delete=models.CASCADE)
+    created_at=models.DateTimeField(auto_now_add=True)
+    is_active=models.IntegerField(default=1)
+
+class ProductVarient(models.Model):
+    title=models.CharField(max_length=255)
+    created_at=models.DateTimeField(auto_now_add=True)
+    
+    
+class ProductVarientItems(models.Model):
+    product_varient_id=models.ForeignKey(ProductVarient,on_delete=models.CASCADE)
+    product_id=models.ForeignKey(Products,on_delete=models.CASCADE)
+    title=models.CharField(max_length=255)
+    created_at=models.DateTimeField(auto_now_add=True)
+
+class CustomerOrders(models.Model):
+    product_id=models.ForeignKey(Products,on_delete=models.DO_NOTHING)
+    purchase_price=models.CharField(max_length=255)
+    coupon_code=models.CharField(max_length=255)
+    discount_amt=models.CharField(max_length=255)
+    product_status=models.CharField(max_length=255)
+    created_at=models.DateTimeField(auto_now_add=True)
+
+class OrderDeliveryStatus(models.Model):
+    order_id=models.ForeignKey(CustomerOrders,on_delete=models.CASCADE)
+    status=models.CharField(max_length=255)
+    status_message=models.CharField(max_length=255)
+    created_at=models.DateTimeField(auto_now_add=True)
+    updated_at=models.DateTimeField(auto_now_add=True)
