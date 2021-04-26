a2092b17abef0faad0ba452603301447a31e4d22
Gateway47 <itsregalo047@gmail.com>
2021-04-26 18:31:30 +0300

    added tinymce to frontend

--- ProductsApp/forms.py
+++ ProductsApp/forms.py
@@ -1,6 +1,10 @@
 from django import forms
 from .models import Products
+from tinymce.widgets import TinyMCE
+
 class ProductForm(forms.ModelForm):
+    product_description = forms.CharField(widget=TinyMCE(attrs={'cols': 80, 'rows':10}))
+    product_long_description = forms.CharField(widget=TinyMCE(attrs={'cols': 80, 'rows':10}))
     class Meta:
         model = Products
         exclude = ["slug","image_thumbnail","created_at", "is_approved","added_by_merchant"]
