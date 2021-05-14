2752d299db5b3101188e49e323e769c1d8a75223
Gateway47 <itsregalo047@gmail.com>
2021-05-14 12:31:21 +0300

    last configs

--- PriceTracker/settings.py
+++ PriceTracker/settings.py
@@ -131,11 +131,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 
-STATIC_ROOT = os.path.join(BASE_DIR,'static_root')
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'frontend/static')
 ]
+
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 
 MEDIA_URL = '/media/'
