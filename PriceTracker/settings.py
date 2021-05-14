3b45509911e52e3e4f7cc8512e44695fa90ad377
Gateway47 <itsregalo047@gmail.com>
2021-05-14 13:51:25 +0300

    staticfiles dir

--- PriceTracker/settings.py
+++ PriceTracker/settings.py
@@ -137,7 +137,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'frontend/static')
 ]
 
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 MEDIA_URL = '/media/'
