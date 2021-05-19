e041cc07cbe80d9a5335f724de2679fb101a75a2
Gateway47 <itsregalo047@gmail.com>
2021-05-19 19:21:44 +0300

    best client reviews

--- User/admin.py
+++ User/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from User.models import (CustomUser, AdminUser, StaffUser,
                          MerchantUser, CustomerUser, PhoneNumber, Profile,
-                         EmailSubscibers)
+                         EmailSubscibers, BestCustomerReviews)
 from django.contrib.auth.admin import UserAdmin
 # Register your models here.
 
@@ -28,3 +28,4 @@ admin.site.register(CustomerUser)
 admin.site.register(PhoneNumber)
 admin.site.register(Profile)
 admin.site.register(EmailSubscibers)
+admin.site.register(BestCustomerReviews)
